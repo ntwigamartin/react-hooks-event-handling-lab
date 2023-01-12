d9a008bfd02ce71020ff5dc3aee9fe2a70a9a2bd
ntwigamartin <ntwiga.martin@gmail.com>
2023-01-12 15:34:02 +0300

    Completed Assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
 // Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
+    function print () {
+        console.log('Entering password...');
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" name="" value="" onChange={print}/>
+
+        </div>
     )
 }
 
